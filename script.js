aeec0425dda4193f76891c1bbc3bfe15d331bbc6
Devansh0309 <93301328+Devansh0309@users.noreply.github.com>
2023-09-25 16:19:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function changeColor(){
+	const id= document.getElementById("input-id").value
+	
+	const color= document.getElementById("color-id").value
+	// alert("hello",id,color)
+	if(parseInt(id)>=1 && parseInt(id)<=9 && color){
+		document.getElementById(id).style.background=color
+		for(let i=1;i<10;i++){
+			if(i!=id){
+				document.getElementById(i).style.background="transparent"
+			}
+		}
+	}
+}
